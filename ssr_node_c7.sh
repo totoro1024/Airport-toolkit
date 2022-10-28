652172c593b5df75000eb3604df635eb57c97c62
totoro0101 <c237802126@gmail.com>
2022-10-28 15:53:12 +0800

    Update ssr_node_c7.sh

--- ssr_node_c7.sh
+++ ssr_node_c7.sh
@@ -93,7 +93,7 @@ echo "Installing Python3.6..."
 yum install python36 python36-pip -y
 echo "Installing Shadowsocksr server from GitHub..."
 mkdir /soft
-cd /tmp && git clone -b testing https://github.com/Anankke/shadowsocks-mod.git
+cd /tmp && git clone -b py2 https://github.com/totoro1024/shadowsocks-mod.git
 mv shadowsocks-mod shadowsocks
 mv -f shadowsocks /soft
 cd /soft/shadowsocks
